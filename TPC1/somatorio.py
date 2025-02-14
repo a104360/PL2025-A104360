132a29574ec2596fdee2d1937c4c12cd90dee2bf
a104360 <a104360@alunos.uminho.pt>
2025-02-14 18:04:23 +0000

    Correção no TPC1; Criação do TPC2

--- TPC1/somatorio.py
+++ TPC1/somatorio.py
@@ -1,11 +1,27 @@
 import sys
 import re
-import os
 
 def main():
     acc = 0
     on = True
-    os.system("clear")
+    inputFileName = input("INSERIR NOME DO FICHEIRO DE INPUT : ")
+    if inputFileName == "\n": inputFile = None
+    else: inputFile = "exists"
+    outputFileName = input("INSERIR NOME DO FICHEIRO DE OUTPUT : ")
+    if outputFileName == "\n": output = None
+    else: output = "exists"
+    
+            
+    if inputFile: 
+        inputFile = open(inputFileName,"r")
+        sys.stdin = inputFile
+
+    if output:
+        output = open(outputFileName,"w")
+        if not output:
+            output = open(outputFileName,"xw")
+        sys.stdout = output
+
 
     for linha in sys.stdin:
         numbers : list[str] = re.findall(r"\bOn\b|Off|\-?\d+|=",linha,re.IGNORECASE)
@@ -16,12 +32,16 @@ def main():
             if a.capitalize() == "On":
                 on = True
                 continue
-            if a == "=":
-                print(acc)
+            if a.find('=') != -1:
+                sys.stdout.write(f">>{acc}\n")
+                sys.stdout.flush()
                 continue
             if on : 
                 acc = acc + int(a)
-
+        
+    sys.stdout.write(f">>{acc}\0")
+    if inputFile : inputFile.close()
+    if output : output.close()
 
 if __name__ == "__main__":
     main()
